b29cf5650684b2c557595e4679390ca122f79a30
hepochan <s.sairenji.23.6.1094@4nonome.com>
2023-08-21 15:43:38 +0900

    [確認課題]制御構文

--- prime_number.py
+++ prime_number.py
@@ -2,3 +2,15 @@ a = input("aの値を入力: ")
 b = input("bの値を入力: ")
 
 # TODO
+c=int(a)
+d=int(b)
+for i in range(2,c):
+    if c%i==0:
+        print("aは素数ではない")
+        break
+print("aは素数")
+for i in range(2,d):
+    if d%i==0:
+        print("bは素数ではない")
+        break
+    print("bは素数")
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -3,3 +3,8 @@ from math import sin
 # print(sin(0))
 # >>> 0
 # -----------
+h=(3.1415/(2*100))#piは3.1415とした
+S=0
+for i in range(1,100):
+    S=S+(h/2)*(sin(h*i-h)+sin(h*i))
+print(S)
